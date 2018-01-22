ffdb2b321c1b3050b1c05f2cb6b9b4bd2b1100d0
Hai Pham <viethai.pham@holmusk.com>
2018-01-22 11:42:40 +0800

    WIP - fix postgresql credentials for test

--- test/middlewares.ts
+++ test/middlewares.ts
@@ -3,7 +3,7 @@ import { PGRequest } from './../src';
 
 export type Req = PGRequest.Self;
 
-export let retryMiddlewareKey = "AddRetry"
+export let retryMiddlewareKey = "AddRetry";
 
-export let retryTransformer: Transformer<Req> = req => req.cloneBuilder()
-  .withRequestRetries(3).build();
+export let retryTransformer: Transformer<Req> = req => 
+  req.cloneBuilder().withRequestRetries(3).build();
